ed18297ffdf5fededac3843863d2e5532560d802
svivek19 <svivek030503@gmail.com>
2024-03-13 18:32:38 +0530

    task finish: todo using various ways

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import ReducerApp from "./ReducerApp";
-// import StateApp from "./StateApp";
-// import Todos from "./Todos";
+import StateApp from "./StateApp";
+import Todos from "./Todos";
 
 export default function App() {
   return (
     <div>
-      {/* <Todos /> */}
-      {/* <StateApp /> */}
+      <h1>Normal approch</h1>
+      <Todos />
+      <hr />
+      <h1>Using useState Hook</h1>
+      <StateApp />
+      <hr />
+      <h1>Using useReducer Hook</h1>
       <ReducerApp />
     </div>
   );
